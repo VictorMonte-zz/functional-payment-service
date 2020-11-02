b7d7a049d6365623d015b82b4c9173ca4ec814b4
Victor Monte <victor.monte@nubank.com.br>
2020-11-02 17:50:29 -0300

    Add dependencies

--- project.clj
+++ project.clj
@@ -15,7 +15,12 @@
                  [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
                  [org.slf4j/jul-to-slf4j "1.7.26"]
                  [org.slf4j/jcl-over-slf4j "1.7.26"]
-                 [org.slf4j/log4j-over-slf4j "1.7.26"]]
+                 [org.slf4j/log4j-over-slf4j "1.7.26"]
+
+                 [prismatic/schema "1.1.12"]
+                 [com.stuartsierra/component "1.0.0"]
+                 [com.datomic/datomic-pro "0.9.5951"]
+                 [prismatic/schema-generators "0.1.3"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
