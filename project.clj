2246ffbb6867656e8984ba6b7a8e3ef69299cda6
Victor Monte <victorbcm@hotmail.com>
2020-11-15 14:10:37 -0300

    Alter to datomic pro

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                   :creds :gpg}}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [io.pedestal/pedestal.service "0.5.8"]
 
